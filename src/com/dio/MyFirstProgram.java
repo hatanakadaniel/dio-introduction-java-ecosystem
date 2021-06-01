8272e4c04a8fb3af775e5c3de6ec58fcc4283c97
dhatanaka <dhatanaka@uolinc.com>
2021-05-31 21:27:47 -0300

    aula 2

--- src/com/dio/MyFirstProgram.java
+++ src/com/dio/MyFirstProgram.java
@@ -2,10 +2,12 @@ package com.dio;
 
 import com.dio.base.Order;
 
+import java.math.BigDecimal;
+
 public class MyFirstProgram {
 
-	public static void main(String[] args) {
-		final Order order = new Order("code1234");
-		System.out.println(order);
-	}
+    public static void main(String[] args) {
+        final Order order = new Order("code1234", new BigDecimal("99.99"));
+        System.out.println(order);
+    }
 }
--- src/com/dio/base/Order.java
+++ src/com/dio/base/Order.java
@@ -1,18 +1,15 @@
 package com.dio.base;
 
+import java.math.BigDecimal;
+
 public class Order {
 
-	private final String code;
+    private final String code;
+    private final BigDecimal totalValue;
 
-	public Order(String code) {
-		this.code = code;
-	}
-
-	@Override
-	public String toString() {
-		return "Order={" +
-				"code='" + code + "'" +
-				"}";
-	}
+    public Order(String code, BigDecimal totalValue) {
+        this.code = code;
+        this.totalValue = totalValue;
+    }
 
 }
