98382486cca24abac2fe33dda3fcc159da34e2bb
Daniel Hatanaka <hatanakadaniel@gmail.com>
2021-06-16 00:18:34 -0300

    Aula 1 - first commit

--- src/com/dio/base/Order.java
+++ src/com/dio/base/Order.java
@@ -2,17 +2,17 @@ package com.dio.base;
 
 public class Order {
 
-	private final String code;
+    private final String code;
 
-	public Order(String code) {
-		this.code = code;
-	}
+    public Order(String code) {
+        this.code = code;
+    }
 
-	@Override
-	public String toString() {
-		return "Order={" +
-				"code='" + code + "'" +
-				"}";
-	}
+    @Override
+    public String toString() {
+        return "Order={" +
+                "code='" + code + "'" +
+                "}";
+    }
 
 }
